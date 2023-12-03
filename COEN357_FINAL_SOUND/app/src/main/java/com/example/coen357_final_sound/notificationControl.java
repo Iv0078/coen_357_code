7579446d7728c25dc2c54a108be5ab1fa9250e73
Iv0078 <110433667+Iv0078@users.noreply.github.com>
2023-12-03 12:12:42 -0500

    feat: notification control

---
+++ COEN357_FINAL_SOUND/app/src/main/java/com/example/coen357_final_sound/notificationControl.java
@@ -0,0 +1,14 @@
+package com.example.coen357_final_sound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class notificationControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_control);
+    }
+}
